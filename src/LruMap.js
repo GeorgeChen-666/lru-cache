0fc01fcce14f239287ac8013732ed2e76ba032b2
Gerd Neudert <23189582+gneu77@users.noreply.github.com>
2018-12-02 18:11:40 +0100

    small improvement in LRU logic, but without performance gain

--- src/LruMap.js
+++ src/LruMap.js
@@ -1,35 +1,39 @@
 
 /* eslint-disable no-param-reassign */
+const addNewest = (entry, entryList) => {
+  if (entryList.newest === null) {
+    entryList.newest = entry;
+    entryList.oldest = entry;
+  }
+  else {
+    entryList.newest.next = entry;
+    entry.prev = entryList.newest;
+    entryList.newest = entry;
+  }
+};
+
+/* eslint-disable no-param-reassign */
 const makeNewest = (entry, entryList) => {
   if (entry === entryList.newest) {
     // entry already is newest
     return;
   }
-  if (entryList.newest === null) {
-    // first entry
-    entryList.newest = entry;
-    entryList.oldest = entry;
-    return;
+  entryList.newest.next = entry;
+  if (entry.prev === null) {
+    // entry is current oldest (and not newest)
+    entry.prev = entryList.newest;
+    entry.next.prev = null;
+    entryList.oldest = entry.next;
+    entry.next = null;
   }
-  const eNext = entry.next;
-  const ePrev = entry.prev;
-  const oldNewest = entryList.newest;
-  entry.next = null;
-  entry.prev = entryList.newest;
+  else {
+    // entry is neither newest, nor oldest
+    entry.prev.next = entry.next;
+    entry.next.prev = entry.prev;
+    entry.prev = entryList.newest;
+    entry.next = null;
+  }
   entryList.newest = entry;
-  if (eNext !== null) {
-    eNext.prev = ePrev;
-    if (entryList.oldest === entry) {
-      entryList.oldest = eNext;
-    }
-  }
-  if (oldNewest !== null) {
-    oldNewest.next = entry;
-    entry.prev = oldNewest;
-  }
-  if (ePrev !== null) {
-    ePrev.next = eNext;
-  }
 };
 
 /* eslint-disable no-param-reassign */
@@ -128,13 +132,15 @@ export function LruMap(maxSize = null) {
         prev: null,
       };
       keyToEntry.set(key, entry);
+      addNewest(entry, entryList);
+      const removals = shrinkToMaxSize(sizeLimit, entryList, keyToEntry);
+      return removals.length === 1 ? removals[0] : null;
     }
     else {
       entry.value = value;
+      makeNewest(entry, entryList);
+      return null;
     }
-    makeNewest(entry, entryList);
-    const removals = shrinkToMaxSize(sizeLimit, entryList, keyToEntry);
-    return removals.length === 1 ? removals[0] : null;
   };
 
   /** Return value for key (undefined if not exists).
